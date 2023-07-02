0dc10eb7a41499828eb1dc9103de2a39ca2ffbc7
zekraquraan <zekraquraan7@gmail.com>
2023-07-02 22:43:04 +0300

    10:42

--- we.py
+++ we.py
@@ -1,186 +1,186 @@
 from selenium import webdriver
-import time
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+import time
+from bs4 import BeautifulSoup
+from tkinter import Tk, Label, Entry, Button
+from PIL import ImageTk, Image
 
 
-web = webdriver.Chrome()
-web.get('https://ncrc.moj.gov.jo/')
+# Start the Chrome browser and navigate to the URL
+driver = webdriver.Chrome()
+driver.get('https://ncrc.moj.gov.jo/')
+
+# Wait for the nationality dropdown to be clickable
+wait = WebDriverWait(driver, timeout=20)
+nationality_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cph_Base_cph_Child_ddlNationalityRequest_chosen"]/a/div')))
+nationality_input.click()
+
+nationality_options = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="cph_Base_cph_Child_ddlNationalityRequest_chosen"]/a/div')))
+for option in nationality_options:
+    if option.text == 'اردني':
+        option.click()
+        break
+
+# Wait for the national number input field to be visible
+ide_input = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ctl00_ctl00_cph_Base_cph_Child_txtNationalNumberRequest"]')))
+ide_input.clear()
+ide_input.send_keys('9912016973')
+
+# Select the language from the dropdown
+language_input = driver.find_element(By.ID, 'cph_Base_cph_Child_ddlLanguage')
+language_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//option[text()='عربي']")))
+language_option.click()
+
+date_of_birth = wait.until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cph_Base_cph_Child_dtpBirthDate_dateInput')))
+date_string = '1991/05/04'
+driver.execute_script("arguments[0].setAttribute('value', arguments[1])", date_of_birth, date_string)
+driver.execute_script("arguments[0].dispatchEvent(new Event('change'))", date_of_birth)
+
+
 time.sleep(2)
 
-Nationality = 'اردني'
-nation= web.find_element_by_xpath('//*[@id="cph_Base_cph_Child_ddlNationalityRequest_chosen"]')
+# Wait for the first name input field to be visible
+first_name_input = wait.until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cph_Base_cph_Child_txtFirstName')))
+first_name = 'ذكرى'
+driver.execute_script("arguments[0].value = arguments[1]", first_name_input, first_name)
+driver.execute_script("arguments[0].dispatchEvent(new Event('change'))", first_name_input)
+time.sleep(2)
 
-nation.send_keys(Nationality)
+# Enter the last name
+last_name_input = wait.until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cph_Base_cph_Child_txtFamilyName')))
+last_name = 'قرعان'
+driver.execute_script("arguments[0].value = arguments[1]", last_name_input, last_name)
+driver.execute_script("arguments[0].dispatchEvent(new Event('change'))", last_name_input)
+time.sleep(2)
 
-the_ID_number = 9912016973
-ide = web.find_element_by_class_name('riSingle RadInput RadInput_Default RadInputRTL RadInputRTL_Default')
+certifcated_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cph_Base_cph_Child_ddlPurpose"]/option[4]')))
+certifcated_input.click()
 
-ide.send_keys(the_ID_number)
+certifacted_options = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="cph_Base_cph_Child_ddlPurpose"]/option[4]')))
+for option in certifacted_options:
+    if option.text == 'جهات داخلية':
+        option.click()
+        break
 
+time.sleep(2)
 
+issued_from = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="cph_Base_cph_Child_ddlCertificateFrom_chosen"]/a/span')))
+driver.execute_script("arguments[0].scrollIntoView(true);", issued_from)
+driver.execute_script("arguments[0].click();", issued_from)
 
+# Wait for the dropdown to be visible and contain options
+# wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="cph_Base_cph_Child_ddlCertificateFrom_chosen"]/div/ul/li')))
 
-bdate=4/5/1991
-birth=web.find_element_by_class_name('riTextBox riEnabled')
-bdate.send_keys(bdate)
+# # Click the dropdown to open the options
+# issued_options = driver.find_elements(By.XPATH, '//*[@id="cph_Base_cph_Child_ddlCertificateFrom_chosen"]/div/ul/li')
+# issued_options[0].click()
+# time.sleep(2)
+# driver.execute_script("arguments[0].scrollIntoView(true);", issued_from)
 
-lang='عربي'
-language=web.find_element_by_xpath('//*[@id="cph_Base_cph_Child_ddlLanguage"]/option[3]')
-lang.send_keys(lang)
+wait = WebDriverWait(driver, 10)
+mobile_input = wait.until(EC.presence_of_element_located((By.ID, 'cph_Base_cph_Child_txtSms')))
+mobile_input.clear()  # Clear any existing value
+mobile_input.send_keys('0777924253')
+time.sleep(2)
 
-
-
-
-first="ذكرى"
-firstName=web.find_element_by_xpath('//*[@id="ctl00_ctl00_cph_Base_cph_Child_txtFirstName"]')
-firstName.send_keys(first)
-
-
-
-last="قرعان"
-lastName=web.find_element_class_name('ctl00$ctl00$cph_Base$cph_Child$txtFamilyName')
-lastName.send_keys(last)
-
-purpose='جهات خارجية'
-pu=web.find_element_class_name('جهات خارجية')
-pu.send_keys(purpose)
-
-fromwh ='محكمة بداية اربد'
-fromWHere=web.find_element_class_name('محكمة بداية اربد')
-fromWHere.send_keys(fromwh)
-
-mobile =777924253
-mobileN=web.find_element_class_name('form-control intlTelInput')
-mobileN.send_keys(mobile)
-
-email ='zekraquraan7@gmail.com'
-email1=web.find_element_class_name('riTextBox riEnabled')
-email1.send_keys(email)
-
-code =''
-code1=web.find_element_by_id('ctl00_ctl00_cph_Base_cph_Child_CaptchaCodeTextBox')
-code1.send_keys(code)
+email_input = wait.until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cph_Base_cph_Child_txtEmail')))
+e_mail = 'zekraquraan7@gmail.com'
+driver.execute_script("arguments[0].value = arguments[1]", email_input, e_mail)
+driver.execute_script("arguments[0].dispatchEvent(new Event('change'))", email_input)
+time.sleep(2)
 
 
+info_element = driver.find_element(By.XPATH, '//*[@id="ctl00_ctl00_cph_Base_cph_Child_captcha_CaptchaImage"]')
+info_screenshot = info_element.screenshot_as_png
+with open('info_screenshot.png', 'wb') as file:
+            file.write(info_screenshot)
 
 
+window = Tk()
+window.title("Captcha Input")
+window.geometry("300x150")
+
+# Load the screenshot image
+image = Image.open('info_screenshot.png')
+photo = ImageTk.PhotoImage(image)
+image_label = Label(window, image=photo)
+image_label.pack()
+
+# Create an input field
+input_field = Entry(window)
+input_field.pack()
+
+def submit():
+    user_input = input_field.get()
+    # Do something with the user input
+    print("User input:", user_input)
+    # Close the GUI window after submitting
+    window.destroy()
+# Create a button to submit the input
+submit_button = Button(window, text="Submit", command=submit)
+submit_button.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
+
+# Use the user input in the Selenium code
+captcha_input = wait.until(EC.presence_of_element_located((By.ID, 'ctl00_ctl00_cph_Base_cph_Child_CaptchaCodeTextBox')))
+captcha_input.send_keys(user_input)
+# window = Tk()
+# image = Image.open('ws.png')
+# photo = ImageTk.PhotoImage(image)
+# image_label = Label(window, image=photo)
+# image_label.pack()
+# input_field = Entry(window)
+# input_field.pack()
 
 
-submit_button = web.find_element_by_xpath('//*[@id="cph_Base_cph_Child_btnRequest"]')
+# def submit():
+#     user_input = input_field.get()
+#     # Do something with the user input
+#     print("User input:", user_input)
+
+# # Create a button to submit the input
+# submit_button = Button(window, text="Submit", command=submit)
+# submit_button.pack()
+
+# # Start the Tkinter event loop
+# window.mainloop()
 
 
+# info_element = driver.find_element(By.XPATH, '//*[@id="ctl00_ctl00_cph_Base_cph_Child_captcha_CaptchaImage"]')
+# info_screenshot = info_element.screenshot_as_png
+# with open('ws.png', 'wb') as file:
+#    file.write(info_screenshot)
 
+# html = '''
+# <img id="ctl00_ctl00_cph_Base_cph_Child_captcha_CaptchaImage" alt="" src="Telerik.Web.UI.WebResource.axd?type=rca&amp;isc=true&amp;guid=76ba5c60-31b6-4431-bfc4-da15ef85f847" style="height:50px;width:180px;display:block;">
+# <input id="ctl00_ctl00_cph_Base_cph_Child_CaptchaCodeTextBox" name="ctl00$ctl00$cph_Base$cph_Child$CaptchaCodeTextBox" size="20" maxlength="6" class="riTextBox riEnabled" onpaste="return false" type="text" value="" autocomplete="off">
+# '''
 
-# # Wait for the element with the specified class name to be visible
-# wait = WebDriverWait(driver, 10)
-# element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".chosen-single")))
+# soup = BeautifulSoup(html, 'html.parser')
 
-# # Click on the element to open the dropdown
-# element.click()
+# # Extract the image source
+# image_src = soup.find('img', id='ctl00_ctl00_cph_Base_cph_Child_captcha_CaptchaImage')['src']
+# print("Image Source:", image_src)
 
-# # Find the option with the desired value and select it
-# option = wait.until(EC.visibility_of_element_located((By.XPATH, "//li[contains(text(), 'اردني')]")))
-# option.click()
+# # Extract the input element's ID and class
+# input_id = soup.find('input', id='ctl00_ctl00_cph_Base_cph_Child_CaptchaCodeTextBox')['id']
+# input_class = soup.find('input', id='ctl00_ctl00_cph_Base_cph_Child_CaptchaCodeTextBox')['class']
+# print("Input ID:", input_id)
+# print("Input Class:", input_class)
 
-# Extract the selected value (optional)
-# selected_value = option.text
-# print("Selected value:", selected_value)
+# Find and click the submit button
+submit_button = driver.find_element(By.XPATH, '//*[@id="cph_Base_cph_Child_btnRequest"]')
+submit_button.click()
 
-# # Find the element with the specified ID
-# national_number_field = wait.until(EC.visibility_of_element_located((By.ID, "ctl00_ctl00_cph_Base_cph_Child_txtNationalNumberRequest")))
+# Wait for the page to load
 
-# # Clear the field and enter the desired value
-# national_number_field.clear()
-# national_number_field.send_keys("9912016973")
-
-# # Wait for the language dropdown to be visible
-# language_dropdown = wait.until(EC.visibility_of_element_located((By.ID, "cph_Base_cph_Child_ddlLanguage")))
-
-# Select the desired language
-
-
-# user_name = 9912016973
-# user_pass = "ctl00_ctl00_cph_Base_cph_Child_txtNationalNumberRequest"
-
-# user_name_input_field = driver.find_element(By.ID, "ctl00_ctl00_cph_Base_cph_Child_txtNationalNumberRequest")
-# user_name_input_field.clear()
-# user_name_input_field.send_keys(user_name)
-
-
-# language_select = Select(language_dropdown)
-# language_select.select_by_visible_text("عربي")
-
-# # Find the first name field
-# first_name_field = wait.until(EC.visibility_of_element_located((By.ID, "ctl00_ctl00_cph_Base_cph_Child_txtFirstName")))
-# if first_name_field.is_enabled():
-#     first_name_field.send_keys("zekra")
-# else:
-#     print("The first name field is disabled.")
-
-
-# # Enter the first name
-
-
-# # Find the family name field
-# family_name_field = wait.until(EC.visibility_of_element_located((By.ID, "ctl00_ctl00_cph_Base_cph_Child_txtFamilyName")))
-
-# # Enter the family name
-# family_name_field.send_keys("quraan")
-
-# # Wait for the purpose dropdown to be visible
-# purpose_dropdown = wait.until(EC.visibility_of_element_located((By.ID, "cph_Base_cph_Child_ddlPurpose")))
-
-# # Select the desired purpose
-# purpose_select = Select(purpose_dropdown)
-# purpose_select.select_by_visible_text("جهات خارجية")
-
-# # Wait for the options to be present
-# options_locator = (By.XPATH, "//select[@id='cph_Base_cph_Child_ddlCourt']//option")
-# options = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(options_locator))
-
-# # Find the desired option and click on it
-# desired_option_text = "محكمة بداية اربد"
-# for option in options:
-#     if option.text == desired_option_text:
-#         option.click()
-#         break
-
-# # Find the SMS field
-# sms_field = wait.until(EC.visibility_of_element_located((By.ID, "cph_Base_cph_Child_txtSms")))
-
-# # Enter the SMS value
-# sms_field.send_keys("777924253")
-
-# # Find the email field
-# email_field = wait.until(EC.visibility_of_element_located((By.ID, "ctl00_ctl00_cph_Base_cph_Child_txtEmail")))
-
-# # Enter the email value
-# email_field.send_keys("zekraquraan7@gmail.com")
-
-# # Find the captcha field
-# captcha_field = wait.until(EC.visibility_of_element_located((By.ID,"ctl00_ctl00_cph_Base_cph_Child_CaptchaCodeTextBox")))
-
-
-# captcha_field.send_keys("5FXPR")
-
-# submit_button = driver.find_element_by_id("cph_Base_cph_Child_btnRequest")
-# submit_button.click()
-
-
-# wait.until(EC.url_contains("https://ncrc.moj.gov.jo/"))  
-
-# Get the page source
+# Get the page source (optional)
 # page_source = driver.page_source
+# print(page_source)
 
-# # Print all contents
-# print("Page Contents:")
-# print("https://ncrc.moj.gov.jo/")
-
-# import time
-# time.sleep(30) 
 # Close the browser
-
+# driver.quit()
